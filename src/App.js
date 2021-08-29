88bf09b9b216e73d62f40c66776e5a03b792a1b6
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-08-29 20:57:49 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import React from 'react';
+import { useState } from "react";
 
 function App() {
-  
+  const [count, setCount] = useState(0)
   const name = 'hello world'
 
   const handleClick = () => {
-  console.log(name)
+    setCount(count + 1)
+    console.log(name)
   }
+
   return (
     <div className="App">
     <h1>テキスト１</h1>
         <button onClick={handleClick}>あ</button>
     <h1>テキスト2</h1>
         <button onClick={() => { alert('🍣') }}>い</button>
-    <h1>テキスト3</h1>
-        <button onClick>iine</button>
-    </div>
-  );
+  <h1>テキスト3</h1>
+  <p>{count}</p>
+  <button onClick={handleClick}>eene</button>
+</div>
+)
 }
 
 export default App;
